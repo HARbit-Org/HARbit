bc7a6650aa202636593f24a900aa137374efd945
Andre Pilco <andrepilcoch@gmail.com>
2025-09-21 20:53:51 -0500

    following color palette

--- mobile/src/main/java/com/example/harbit/MainActivity.kt
+++ mobile/src/main/java/com/example/harbit/MainActivity.kt
@@ -11,7 +11,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HARbitTheme {
+            HARbitTheme (
+                darkTheme = false,
+                dynamicColor = false
+            ) {
                 val navController = rememberNavController()
                 AppNavigation(
                     navController = navController,
